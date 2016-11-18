2c1bfb39843f96b76b0683f3c63d5174cb2bf99f
songyang <1916334303@qq.com>
2016-11-18 11:14:55 +0800

    qqq

---
+++ app/scripts/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name hahaApp
+ * @description
+ * # hahaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('hahaApp', [])
+  		/*app.filter("sub",function(){
+			return function(e){
+				if(e.length<=10){
+					return e
+				}else{
+					var arr = []
+					arr.push(e.substring(0,10))
+					var arr1 = (e.length)-(arr.join("").length)
+					return arr+"..."
+				}
+			}
+		}),
+*/
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hahaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hahaApp
+ */
+angular.module('hahaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
